851cd0917e3d529e70f7595c656e652ed10f1834
RVirmoors <prowlergrig@yahoo.ie>
2021-04-19 22:52:11 +0300

    osc-rot

--- osc-unity/Assets/OscToRotation.cs
+++ osc-unity/Assets/OscToRotation.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-//using BlobHandles;
 using UnityEngine;
 
 namespace OscCore.Demo
@@ -11,16 +10,32 @@ namespace OscCore.Demo
         float x, y, z;
 
         void Awake() {
-            //Receiver = GameObject.Find("Directional Light").GetComponent<OscReceiver>();
-            Receiver.Server.TryAddMethod("/accelerometer/x", ReadValues);
+            Receiver.Server.TryAddMethod("/accelerometer/x", ReadX);
+            Receiver.Server.TryAddMethod("/accelerometer/y", ReadY);
+            Receiver.Server.TryAddMethod("/accelerometer/z", ReadZ);
         }
-        void ReadValues(OscMessageValues values)
+        void ReadX(OscMessageValues values)
         {
             x = values.ReadFloatElement(0);
         }
+        void ReadY(OscMessageValues values)
+        {
+            y = values.ReadFloatElement(0);
+        }
+        void ReadZ(OscMessageValues values)
+        {
+            z = values.ReadFloatElement(0);
+        }
 
         void Update() {
-            transform.Rotate(x, 0, 0);
+            float pitch, roll;
+            int sign = (z > 0) ? 1 : -1;
+            float miu = 0.001f;
+            
+            pitch = -90 + Mathf.Atan2(-x, Mathf.Sqrt(y*y + z*z)) * 180/Mathf.PI;
+            roll  = -90 + Mathf.Atan2(y, sign* Mathf.Sqrt(z*z+ miu*x*x)) * 180/Mathf.PI; 
+
+            transform.eulerAngles = new Vector3(0f, pitch, roll);
         }
     }
 }
